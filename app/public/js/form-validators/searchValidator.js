75fd6d4d7b30977d3ad7b2ed8d5e33c70ed632e7
CDkeyZhoukay <31775804+CDkeyZhoukay@users.noreply.github.com>
2017-09-13 12:50:57 -0300

    Add files via upload

---
+++ app/public/js/form-validators/searchValidator.js
@@ -0,0 +1,27 @@
+
+function SearchValidator()
+{
+// bind a simple alert window to this controller to display any errors //
+	this.loginErrors = $('.modal-alert');
+	
+	this.showLoginError = function(t, m)
+	{
+		$('.modal-alert .modal-header h4').text(t);
+		$('.modal-alert .modal-body p').html(m);
+		this.loginErrors.modal('show');
+	}
+}
+
+SearchValidator.prototype.validateForm = function()
+{	
+	var cityName = $('#city-tf').val();
+
+	if (cityName.trim() == ''){
+		this.showLoginError('ERROR', 'Please enter a valid city name');
+		return false;
+	}	else{
+		return true;
+	}		
+
+}
+
