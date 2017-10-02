cffc7e91d68d248b0e04138261f1b2099434f754
CDkeyZhoukay <31775804+CDkeyZhoukay@users.noreply.github.com>
2017-10-01 21:50:33 -0300

    Update search.js

--- app/public/js/views/search.js
+++ app/public/js/views/search.js
@@ -3,12 +3,17 @@ $(document).ready(function(){
 
 	var sc = new SearchController();
 	var sv = new SearchValidator();
+
 // main search form //
 	$('#search').ajaxForm({
 		beforeSubmit : function(formData, jqForm, options){
 			if (sv.validateForm() == false){				
 				return false;
 			} 	else{	
+				var searchButton = $('#btn-search').data('clicked');
+				if (searchButton == false){
+					formData[0].value = (sc.SelectRandomCity());
+				}
 				return true;
 			}
 		},
