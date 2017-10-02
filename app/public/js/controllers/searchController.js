ad38c3f6f3d45c63c8f046b629bed2f46a71cedb
CDkeyZhoukay <31775804+CDkeyZhoukay@users.noreply.github.com>
2017-10-01 21:49:52 -0300

    Update searchController.js

--- app/public/js/controllers/searchController.js
+++ app/public/js/controllers/searchController.js
@@ -1,17 +1,47 @@
 
-function SearchController()
-{
-
-// bind event listeners to button clicks //
+function SearchController(){
+	// bind event listeners to button clicks //
 	var that = this;		
-	
+	var cityList = [];
+
 // switch to user setting //
 	$('#btn-account-setting').click(function(){
-		window.location.href = '/home';});	
+		window.location.href = '/home';
+	});	
 	
 // handle user logout //
-	$('#btn-logout-search').click(function(){ that.attemptLogout(); });
-	
+	$('#btn-logout-search').click(function(){ 
+		that.attemptLogout(); 
+	});
+
+// button 'I'm feeling lucky' clicked //
+	$('#btn-lucky').click(function(){
+		$('#btn-search').data('clicked', false);
+
+		if (localStorage.getItem("cityList") == null){
+		   $.ajax({
+			   async: false,
+			   url: './data/city-list.json',
+			   dataType: 'json',
+			   success: function(data) {
+			      $.each(data, function(key, val) {
+			        cityList.push(val);    
+			      });   	
+			   }
+			});  	
+
+			localStorage.setItem("cityList", JSON.stringify(cityList));				
+		}else{
+			cityList = JSON.parse(localStorage.getItem("cityList"));
+		}
+		
+	});
+
+// button 'Search' clicked //
+	$('#btn-search').click(function(){
+		$(this).data('clicked', true);
+	});	
+
 	this.attemptLogout = function()
 	{
 		var that = this;
@@ -36,7 +66,15 @@ function SearchController()
 		$('.modal-alert button').click(function(){window.location.href = '/';})
 		setTimeout(function(){window.location.href = '/';}, 3000);
 	}		
+
+	this.getRandomCity = function(){
+		var that = this;
+		var random = Math.floor(Math.random() * 3166);
+		return cityList[random].name;
+	}	
 }
+
+
 
 SearchController.prototype.onSearchResult = function(i)
 {
@@ -45,4 +83,33 @@ SearchController.prototype.onSearchResult = function(i)
 	$('.modal-alert .modal-body p').html('It is ' + i.main.temp + ' degrees in ' + i.name);
 	$('.modal-alert').modal('show');
 	$('.modal-alert button').off('click');
+}
+
+SearchController.prototype.SelectRandomCity = function()
+{
+	//Random select a city for 'I'm feeling lucky'
+
+	var that = this;
+	var randomCity = that.getRandomCity();
+
+	//Check this city name have been searched before or not
+	var cityName = JSON.parse(sessionStorage.getItem("cityName"));
+	if (cityName != null){
+		if (typeof(cityName) == 'string'){
+			cityName = [JSON.parse(sessionStorage.getItem("cityName"))];	
+		}
+
+		while(cityName.indexOf(randomCity) > -1){
+			randomCity = that.getRandomCity();				
+		}
+
+		//Add to the array and save to session storage
+		cityName.push(randomCity);
+		sessionStorage.setItem("cityName", JSON.stringify(cityName));		
+	}else{
+		//Save to session storage
+		sessionStorage.setItem("cityName", JSON.stringify(randomCity));
+	}
+		
+	return randomCity;
 }
